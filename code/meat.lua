affd57115b5acfadf7a2f8bf83ee1f0dffec5bdf
Sabrina112358 <sabrina.marques@unesp.br>
2021-09-06 10:47:44 -0300

    Update code

--- code/meat.lua
+++ code/meat.lua
@@ -2,24 +2,37 @@
 # An Agent-Based Model to Simulate Meat Consumption Behaviour of Consumers in Britain
 --]]
 
+--[[
+Agents atributes:
+@SSerial: respondent survey ID;
+@Rsex: 1 = M, 2 = F;
+@Rage: consumer's age;
+@Country: country of origin (1=England, 2=Scotland, 3=Wales);
+@MeatEnv: concerns related to the impact of meat on the Environment;
+@MeatHlth: concerns related to the impact of meat on the Health;
+@MeatWelf: concerns related to the impact of meat on the Animal Welfare;
+@livcost1: price sensitivity;
+@ag.ped: Price elasticity demand associated with the income level;
+@fam_alpha: personal susceptibility towards other family members;
+@work_alpha: personal susceptibility towards other colleagues;
+@eat_meat_t0: probability to consume a meal based on meat at t = 0;
+--]]
+
+-- Constantes
+b = {-6.321, 0.655, 0.016, 0.287, 0.623, 0.178, 0.101}
+
 -- Criar a sociedade:
 agent = Agent{
-    -- Atributos extras:
-    -- ag.ped: Price elasticity demand associated with the income level (see Tiffin at al, 2011)
     ped = 0,
-
-    -- fam_alpha: personal susceptibility towards other family members
     fam_alpha  = 0 + Random{mean = 0.15, sd = 0.05}:sample(),
-
-    -- work.alpha: Personal susceptibility towards other colleagues
     work_alpha  = 0 + Random{mean = 0.08, sd = 0.01}:sample(),
 
-    execute = function()
-        --[[if agent.REarnD >= 0 and agent.REarnD < 3 then
+    execute = function(agent)
+        if society:get(agent.id).REarnD >= 0 and society:get(agent.id).REarnD < 3 then
             agent.ped = 0.839
         else
             agent.ped = 0.804
-        end -- ped --]]
+        end -- ped
 
         if agent.fam_alpha < 0 then
             agent.fam_alpha = 0
@@ -34,21 +47,26 @@ agent = Agent{
         elseif agent.work_alpha > 0.12 then
             agent.work_alpha = 0.12
         end -- if work
+
+        -- prob-at-time-zero: probability of eating meat at time zero
+        local y = ( b[1] + (b[2] * society:get(agent.id).Rsex) + (b[3] * society:get(agent.id).Rage) + (b[4] * society:get(agent.id).MeatEnv) + (b[5] * society:get(agent.id).MeatHlth) + (b[6] * society:get(agent.id).MeatWelf) + (b[7] * society:get(agent.id).livcost1) * (1 / 100))
+        local above = math.exp(y)
+        local below = (1 + math.exp(y))
+        agent.eat_meat_t0 = (1 - (above / below)) * society:get(agent.id).MeatHab
     end, -- execute
+
     run = function() end,
 }
 
 society = Society{
-    file ="D:/documentos/INPE/modelagem/projeto_final/modelo_Terrame/dadosFiltrados.csv",
+    file ="D:/documentos/INPE/modelagem/projeto_final/modelo_Terrame/data/dadosFiltrados.csv",
     sep = ",", -- default
     source = "csv",
-    instance = agent,
-    quantity = 50
+    instance = agent
 }
 
 society:execute() -- call execute for each agent
 society:run() -- call run for each agent
 
 print(#society)
-
-print(society.agent[1])
+print(society:get(2))
---
+++ meat.lua
@@ -0,0 +1,1 @@
+Added another line to meat.lua
