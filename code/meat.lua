a57b86e37986a95487442f493e2ce51ac72974fc
Matheus Elis <matheuselis@id.uff.br>
2021-09-06 23:08:39 -0300

    Adicionado as funções para fazer os link de work

--- code/meat.lua
+++ code/meat.lua
@@ -17,7 +17,7 @@ Agents atributes:
 @work_alpha: personal susceptibility towards other colleagues;
 @eat_meat_t0: probability to consume a meal based on meat at t = 0;
 --]]
-
+require 'code/workers'
 -- Constants
 b = {-6.321, 0.655, 0.016, 0.287, 0.623, 0.178, 0.101}
 
@@ -26,11 +26,22 @@ b = {-6.321, 0.655, 0.016, 0.287, 0.623, 0.178, 0.101}
 
 -- Criar a sociedade:
 agent = Agent{
+    family_member = false,                       -- A flag to mark those agents who are part of a family
+    agent_family_ID = 0,                    -- Agent's family ID
+    worker = false,                              -- Every one is part of a family, but only some agents are workers
+    team_member = false,                         -- A flag to mark those agents who are part of a work team
+    agent_team_ID = 0,                     -- Agent's team ID
+    eat_meat_t0 = {},
     ped = 0,
     fam_alpha  = 0 + Random{mean = 0.15, sd = 0.05}:sample(),
     work_alpha  = 0 + Random{mean = 0.08, sd = 0.01}:sample(),
-
+    
     execute = function(agent)
+        
+        if agent.REconAct == 1 then 
+            agent.worker = true
+        end
+        local max_team_ID = createWorkerNetWork(society)
         if society:get(agent.id).REarnD >= 0 and society:get(agent.id).REarnD < 3 then
             agent.ped = 0.839
         else
@@ -62,7 +73,7 @@ agent = Agent{
 }
 
 society = Society{
-    file ="D:/documentos/INPE/modelagem/projeto_final/modelo_Terrame/data/sample_100.csv",
+    file ="data/sample_397.csv",
     sep = ",", -- default
     source = "csv",
     instance = agent
@@ -71,5 +82,28 @@ society = Society{
 society:execute() -- call execute for each agent
 society:run() -- call run for each agent
 
-print(#society)
-print(society:get(2))
+agente_id = {}
+eat_meat_t0 = {}
+forEachAgent(society, function(agente)
+    table.insert(agente_id, tonumber(agente.id))
+    table.insert(eat_meat_t0, agente.eat_meat_t0)
+end)
+
+function bubbleSort(A,B)
+    local n = #A
+    local swapped = false
+    repeat
+      swapped = false
+      for i=2,n do   -- 0 based is for i=1,n-1 do
+        if A[i-1] > A[i] then
+          A[i-1],A[i] = A[i],A[i-1]
+          B[i-1],B[i] = B[i],B[i-1]
+          swapped = true
+        end
+      end
+    until not swapped
+  end
+bubbleSort(eat_meat_t0,agente_id)
+for _,n in pairs(agente_id) do
+    print(society:get(n).eat_meat_t0)
+end
---
+++ code/workers.lua
@@ -0,0 +1,36 @@
+function getSocialWorkerNetwork(soc, agent, team_ID)
+	local quant = 1
+    local rs = SocialNetwork{name = 'teste'}
+
+    local mean_team_size = 4
+    local quanti_total = Random{mean = mean_team_size, sd = 0.05}:sample()
+    
+    rs:add(agent)
+    agent.team_member = true
+    agent.agent_team_ID = team_ID
+    
+    --print(team_ID)
+    while quant < quanti_total do
+        local randomagent = soc:sample(agent.ReconAct == 1)
+        if randomagent ~= agent and randomagent.team_member == false then
+            rs:add(randomagent, 1)
+            randomagent.team_member = true
+            randomagent.agent_team_ID = team_ID
+            quant = quant + 1
+        end
+    end
+
+	return rs
+end
+team_ID = 1
+function createWorkerNetWork(soc)
+    
+    forEachAgent(soc, function(agent)
+        if agent.worker == true and agent.team_member == false then
+            getSocialWorkerNetwork(soc, agent, team_ID)
+            team_ID = team_ID + 1
+            -- print(team_ID)
+        end
+    end)
+    return team_ID
+end
